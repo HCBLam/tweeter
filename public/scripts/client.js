a1a7f407fadb33970ab1537bf3871865c60a97d0
Hasan Lam <hcb.lam@gmail.com>
2021-10-06 19:52:12 +0000

    Add function to render all tweets on the page

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -9,23 +9,22 @@ $(() => {
 
 
 
-
   const createTweetElement = function(tweet) {
 
     const $tweet = $(`
       <article class="tweet">
         <header class="tweet-header">
           <div class="persona">
-            <img class="avatar" src="${tweetData.user.avatars}">
-            <p>${tweetData.user.name}</p>
+            <img class="avatar" src="${tweet.user.avatars}">
+            <p>${tweet.user.name}</p>
           </div>
-          <p class="handle">${tweetData.user.handle}</p>
+          <p class="handle">${tweet.user.handle}</p>
         </header>
         <div class="statement">
-          ${tweetData.content.text}
+          ${tweet.content.text}
         </div>
         <footer class="tweet-footer">
-          <div class="date">${timeago.format(tweetData.created_at)}</div>
+          <div class="date">${timeago.format(tweet.created_at)}</div>
           <div class="icons">
             <i class="fas fa-flag"></i>
             <i class="fas fa-retweet"></i>
@@ -38,6 +37,15 @@ $(() => {
   }
 
 
+  const renderTweets = function(tweets) {
+    const $tweetsContainer = $(".tweets-container");
+    // $tweetsContainer.empty();
+
+    for (const tweet of tweets) {
+      const $tweet = createTweetElement(tweet);
+      $tweetsContainer.append($tweet);
+    }
+  }
 
 
 
@@ -47,28 +55,47 @@ $(() => {
 
 
 // Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Marie",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@MCurrie"
+    },
+    "content": {
+      "text": "Nothing in life is to be feared, it is only to be understood."
+    },
+    "created_at": 1561116232227
+  },
+  {
   "user": {
     "name": "Descartes",
-    "avatars": "https://i.imgur.com/nlhLi3I.png",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
     "handle": "@rd"
   },
   "content": {
     "text": "Je pense , donc je suis"
   },
   "created_at": 1633438673665
-}
+  }
+]
 
-const $tweet = createTweetElement(tweetData);
-
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-$('.tweets-container').append($tweet);
-
+renderTweets(data);
 
 
 });
-
 
 
 
